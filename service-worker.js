483d4108e1261106e60d1658e2c81d697447508d
Relkady84 <raedelkady@gmail.com>
2026-05-08 08:20:19 +0300

    1

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  * Version bump = bump CACHE_VERSION below.
  */
 
-const CACHE_VERSION = 'v39-2026-05-07-admin-dark-fixed';
+const CACHE_VERSION = 'v40-2026-05-08-role-badge-text-white';
 const CACHE_SHELL   = `payroll-shell-${CACHE_VERSION}`;
 const CACHE_RUNTIME = `payroll-runtime-${CACHE_VERSION}`;
 
